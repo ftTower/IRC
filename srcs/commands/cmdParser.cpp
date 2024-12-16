fe7715090574553a1ba132777659b114967b77d6
Louise Leciak <leciak.louise@gmail.com>
2024-12-16 08:18:35 +0100

    FIX: PARSER(CMDPARSER):
    inverser ping et pong
    adapter la taille du for a la taille du tableau

--- srcs/commands/cmdParser.cpp
+++ srcs/commands/cmdParser.cpp
@@ -6,7 +6,7 @@
 /*   By: lleciak <lleciak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 11:09:56 by lleciak           #+#    #+#             */
-/*   Updated: 2024/12/13 14:47:18 by lleciak          ###   ########.fr       */
+/*   Updated: 2024/12/16 08:18:04 by lleciak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,9 @@ void	parseCmd(Server &serv, int fd, std::string cmd)
 	
 	std::string cmds[] = {"NICK", "USER", "PING", "PONG", "VERSION", "PASS", "JOIN", "PART", "PRIVMSG", "INVITE", "QUIT", "MODE", "TOPIC", "KICK"};
 	void (*foo[])(Server&,int,const std::string &) = {
-		nick_cmd, user_cmd, ping_cmd, pong_cmd, version_cmd, pass_cmd, join_cmd, part_cmd, privmsg_cmd, invite_cmd, quit_cmd, mode_cmd, topic_cmd, kick_cmd
+		nick_cmd, user_cmd, pong_cmd, ping_cmd, version_cmd, pass_cmd, join_cmd, part_cmd, privmsg_cmd, invite_cmd, quit_cmd, mode_cmd, topic_cmd, kick_cmd
 	};
-	for(size_t i = 0; i < 20; i++)
+	for(size_t i = 0; i < 14; i++)
 		if (cmd.find(cmds[i]) != std::string::npos) {
 			try {
 				foo[i](serv,fd, cmd);}
